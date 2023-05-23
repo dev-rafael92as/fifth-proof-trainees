39eed75192e7e43b96486c356de83089ffe0e800
Rafael Barros de Almeida e Silva <rafa_barros92@hotmail.com>
2023-05-23 16:25:22 -0300

    [feat] Included component modal content

--- src/components/ModalGuarantee/ContentModal/index.jsx
+++ src/components/ModalGuarantee/ContentModal/index.jsx
@@ -1,7 +1,22 @@
 import React from 'react'
+import { Container } from './styles'
+import { CardFreeGuarantee } from '../CardFreeGuarantee'
+import { CardGuarantee } from '../CardGuarantee'
 
-export const ContentModal = () => {
+export const ContentModal = ({extendedWarranty}) => {
+    console.log(extendedWarranty)
   return (
-    <div>ContentModal</div>
+    <Container>
+        <div className='modal-container'>
+            <h3 className='modal-container-title'>Adicione Garantia Estendia</h3>
+            <p className='modal-container-description'>Selecione o prazo de proteção e mantenha o produto protegido por mais tempo.</p>
+        </div>
+        <div className='modal-container-wrapperCards'>
+            <CardFreeGuarantee />
+            {extendedWarranty?.map((warranty, index) => (
+                <CardGuarantee warranty={warranty} key={index}/>
+            ))}
+        </div>
+    </Container>
   )
 }
--- src/components/ModalGuarantee/index.jsx
+++ src/components/ModalGuarantee/index.jsx
@@ -4,8 +4,7 @@ import { Container } from './styles';
 import { useContextCheckout } from '../../hooks/useContext';
 import { useQuery } from '@apollo/client';
 import { EXTENDED_WARRANTY } from '../../graphql/extendedWarranty';
-import { CardGuarantee } from './CardGuarantee';
-import { CardFreeGuarantee } from './CardFreeGuarantee';
+import { ContentModal } from './ContentModal';
 Modal.setAppElement('#root');
 
 export const ModalGuarantee = () => {
@@ -58,16 +57,7 @@ export const ModalGuarantee = () => {
                 }
             }}
         >
-            <div className='modal-container'>
-                <h3 className='modal-container-title'>Adicione Garantia Estendia</h3>
-                <p className='modal-container-description'>Selecione o prazo de proteção e mantenha o produto protegido por mais tempo.</p>
-            </div>
-            <div className='modal-container-wrapperCards'>
-                <CardFreeGuarantee />
-                {warrantys?.map((warranty, index) => (
-                    <CardGuarantee warranty={warranty} key={index}/>
-                ))}
-            </div>
+          <ContentModal extendedWarranty={warrantys} />
         </Modal>
 
     </Container>
